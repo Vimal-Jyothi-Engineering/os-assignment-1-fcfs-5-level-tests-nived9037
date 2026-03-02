2d2cbf9d7d6fe785cbe32c9c345db7a846eee941
nived9037 <nived9037@gmail.com>
2026-03-02 23:02:43 +0530

    Completed assignment project

--- fcfs.c
+++ fcfs.c
@@ -10,21 +10,21 @@ typedef struct {
 } Process;
 
 int main() {
+
     int n;
     scanf("%d", &n);
 
     Process p[n];
 
-    for (int i = 0; i < n; i++) {
+    for(int i=0;i<n;i++)
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
-    }
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].arrival > p[j + 1].arrival) {
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(p[j].arrival > p[j+1].arrival){
                 Process temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+                p[j] = p[j+1];
+                p[j+1] = temp;
             }
         }
     }
@@ -32,8 +32,9 @@ int main() {
     int current_time = 0;
     float total_wt = 0, total_tat = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (current_time < p[i].arrival)
+    for(int i=0;i<n;i++){
+
+        if(current_time < p[i].arrival)
             current_time = p[i].arrival;
 
         p[i].waiting = current_time - p[i].arrival;
@@ -45,15 +46,15 @@ int main() {
         total_tat += p[i].turnaround;
     }
 
-    float avg_wt = total_wt / n;
-    float avg_tat = total_tat / n;
+    float avg_wt = total_wt/n;
+    float avg_tat = total_tat/n;
 
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
         printf("%s %d\n", p[i].pid, p[i].waiting);
 
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
         printf("%s %d\n", p[i].pid, p[i].turnaround);
 
     printf("Average Waiting Time: %.2f\n", avg_wt);
