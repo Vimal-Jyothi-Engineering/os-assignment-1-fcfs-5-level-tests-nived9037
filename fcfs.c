d7338b72b23a4a070cb2e91605470dd503bb47da
nived9037 <nived9037@gmail.com>
2026-03-02 23:38:04 +0530

    Completed assignment project

--- fcfs.c
+++ fcfs.c
@@ -11,55 +11,53 @@ typedef struct {
 int main() {
 
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     Process p[n];
 
-    for(int i=0;i<n;i++)
-        scanf("%s %d %d",p[i].pid,&p[i].at,&p[i].bt);
+    for(int i = 0; i < n; i++)
+        scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
 
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(p[j].at > p[j+1].at){
-                Process t=p[j];
-                p[j]=p[j+1];
-                p[j+1]=t;
+    // Sort by arrival time
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(p[j].at > p[j+1].at) {
+                Process temp = p[j];
+                p[j] = p[j+1];
+                p[j+1] = temp;
             }
         }
     }
 
-    int current_time=0;
-    float total_wt=0,total_tat=0;
+    float total_wt = 0, total_tat = 0;
 
-    for(int i=0;i<n;i++){
+    p[0].wt = 0;
+    p[0].tat = p[0].bt;
 
-        int start_time = current_time;
+    total_wt += p[0].wt;
+    total_tat += p[0].tat;
 
-        if(start_time < p[i].at)
-            start_time = p[i].at;
-
-        p[i].wt = start_time;
+    for(int i = 1; i < n; i++) {
+        p[i].wt = p[i-1].wt + p[i-1].bt;
         p[i].tat = p[i].wt + p[i].bt;
-
-        current_time = start_time + p[i].bt;
 
         total_wt += p[i].wt;
         total_tat += p[i].tat;
     }
 
-    float avg_wt = total_wt/n;
-    float avg_tat = total_tat/n;
+    float avg_wt = total_wt / n;
+    float avg_tat = total_tat / n;
 
     printf("Waiting Time:\n");
-    for(int i=0;i<n;i++)
-        printf("%s %d\n",p[i].pid,p[i].wt);
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].wt);
 
     printf("Turnaround Time:\n");
-    for(int i=0;i<n;i++)
-        printf("%s %d\n",p[i].pid,p[i].tat);
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].tat);
 
-    printf("Average Waiting Time: %.2f\n",avg_wt);
-    printf("Average Turnaround Time: %.2f",avg_tat);
+    printf("Average Waiting Time: %.2f\n", avg_wt);
+    printf("Average Turnaround Time: %.2f", avg_tat);
 
     return 0;
 }
