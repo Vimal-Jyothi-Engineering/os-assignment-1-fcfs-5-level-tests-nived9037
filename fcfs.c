ada147d61b045ecd78fb6a8c444e37ea4e070d3f
nived9037 <nived9037@gmail.com>
2026-03-02 23:31:03 +0530

    Completed assignment project

--- fcfs.c
+++ fcfs.c
@@ -18,36 +18,36 @@ int main() {
     for(int i=0;i<n;i++)
         scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
 
-    // sort by arrival time
+    // Sort by arrival time
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(p[j].at > p[j+1].at){
-                Process t=p[j];
-                p[j]=p[j+1];
-                p[j+1]=t;
+                Process temp = p[j];
+                p[j] = p[j+1];
+                p[j+1] = temp;
             }
         }
     }
 
-    int current_time = 0;
+    int time = 0;
     float total_wt = 0, total_tat = 0;
 
     for(int i=0;i<n;i++){
 
-        if(current_time < p[i].at)
-            current_time = p[i].at;
+        if(time < p[i].at)
+            time = p[i].at;
 
-        p[i].wt = current_time - p[i].at;
+        p[i].wt = time;              // NO subtraction
         p[i].tat = p[i].wt + p[i].bt;
 
-        current_time += p[i].bt;
+        time += p[i].bt;
 
         total_wt += p[i].wt;
         total_tat += p[i].tat;
     }
 
-    float avg_wt = total_wt/n;
-    float avg_tat = total_tat/n;
+    float avg_wt = total_wt / n;
+    float avg_tat = total_tat / n;
 
     printf("Waiting Time:\n");
     for(int i=0;i<n;i++)
